84da90a61a46b136ef1d8df7a19e0ddbdfd11bd7
Inchirah Jabir <102082090+inchirahjabir@users.noreply.github.com>
2023-12-29 21:42:59 +0100

    fix : add article

--- app/pages/addArticle.js
+++ app/pages/addArticle.js
@@ -11,22 +11,59 @@ export default function CreatePost({ isDarkMode }) {
     e.preventDefault();
 
     try {
-      const { data, error } = await supabase.from('articles').upsert([formData]);
+      // Check if a record with the same slug already exists
+      const { data: existingData, error: existingError } = await supabase
+        .from('articles')
+        .select('slug')
+        .eq('slug', formData.slug)
+        .single();
 
-      if (error) {
-        throw new Error('Failed to create/update post');
+      if (existingError) {
+        throw new Error('Error checking existing record:', existingError);
       }
 
-      setMessage(
-        <div className={`max-w-lg mx-auto bg-white rounded-lg p-6 shadow-md mt-4 ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
-          <h2 className={`text-2xl font-semibold ${isDarkMode ? 'text-pink-300' : 'text-pink-800'} mb-4 text-center`}>
-            Article Created/Updated
-          </h2>
-          <p className="text-center">
-            Your article has been successfully created/updated.
-          </p>
-        </div>
-      );
+      if (existingData) {
+        // If a record exists, perform an update
+        const { data: updateData, error: updateError } = await supabase
+          .from('articles')
+          .update([formData])
+          .eq('slug', formData.slug);
+
+        if (updateError) {
+          throw new Error('Failed to update post:', updateError);
+        }
+
+        setMessage(
+          <div className={`max-w-lg mx-auto bg-white rounded-lg p-6 shadow-md mt-4 ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
+            <h2 className={`text-2xl font-semibold ${isDarkMode ? 'text-pink-300' : 'text-pink-800'} mb-4 text-center`}>
+              Article Updated
+            </h2>
+            <p className="text-center">
+              Your article has been successfully updated.
+            </p>
+          </div>
+        );
+      } else {
+        // If no record exists, perform an insert
+        const { data: insertData, error: insertError } = await supabase
+          .from('articles')
+          .insert([formData]);
+
+        if (insertError) {
+          throw new Error('Failed to create post:', insertError);
+        }
+
+        setMessage(
+          <div className={`max-w-lg mx-auto bg-white rounded-lg p-6 shadow-md mt-4 ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
+            <h2 className={`text-2xl font-semibold ${isDarkMode ? 'text-pink-300' : 'text-pink-800'} mb-4 text-center`}>
+              Article Created
+            </h2>
+            <p className="text-center">
+              Your article has been successfully created.
+            </p>
+          </div>
+        );
+      }
     } catch (error) {
       console.error('Error creating/updating post:', error);
 
@@ -48,7 +85,7 @@ export default function CreatePost({ isDarkMode }) {
   const [formData, setFormData] = useState({
     title: '',
     content: '',
-    author: author, // Assuming user has an 'email' field
+    author: author,
     tags: '',
     slug: '',
   });
